7fb578d1f7847ab23b55b15420370577c1e1df12
Joey David <doey08@gmail.com>
2021-01-27 23:28:15 -0500

    test api call working

--- web/src/App.js
+++ web/src/App.js
@@ -1,11 +1,52 @@
+import React from 'react';
+import axios from 'axios';
 
+class MyComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: null,
+      isLoaded: false,
+      items: []
+    };
+  }
+
+  componentDidMount() {
+    let config = {
+      headers: {
+        "Accept": "application/json"
+      }
+    }
+    axios.get('https://icanhazdadjoke.com/',config)
+      .then(result => {
+        console.log(result);
+        this.setState({
+          isLoaded: true,
+          items: result.data.joke
+        });
+      })
+      .catch(error => {
+        console.log(error);
+      })
+  }
+
+  render() {
+    const { error, isLoaded, items } = this.state;
+    if (error) {
+      return <div>Error: {error.message}</div>;
+    } else if (!isLoaded) {
+      return <div>Loading...</div>;
+    } else {
+      return (
+        <p>{items}</p>
+      );
+    }
+  }
+}
 
 function App() {
   return (
-    <div>
-      <h1>Hi</h1>
-      <p>Some text</p>
-    </div>
+    <MyComponent></MyComponent>
   );
 }
 
