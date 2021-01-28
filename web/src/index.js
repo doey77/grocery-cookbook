08405f274197b84e2253c68870d4e4e0b604e596
Joey David <doey08@gmail.com>
2021-01-27 23:00:46 -0500

    index redo

--- web/src/index.js
+++ web/src/index.js
@@ -1,15 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import Navigation from './Navigation'
+import Master from './Master'
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Navigation></Navigation>
-    <App></App>
+    <Master></Master>
   </React.StrictMode>,
   document.getElementById('root')
 );
