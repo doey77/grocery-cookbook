009bab90322b03fdbc4b9973f54f6c7cfd3a4e8d
Joey David <doey08@gmail.com>
2021-01-27 22:18:38 -0500

    material-ui, navbar working

--- web/src/App.js
+++ web/src/App.js
@@ -1,21 +1,13 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
-import Button from 'react-bootstrap/Button';
-import Container from 'react-bootstrap/Container';
-import Jumbotron from 'react-bootstrap/Jumbotron'
 
 
 
 function App() {
   return (
-    <div className="App">
-      <Container>
-      <Jumbotron>
-        <h1>This is my React App!</h1>
-        <img src={logo} className="App-logo" alt="logo" />
-      </Jumbotron>
-      <Button>My button</Button>
-      </Container>
+    <div>
+      <h1>Hi</h1>
+      <p>Some text</p>
     </div>
   );
 }
