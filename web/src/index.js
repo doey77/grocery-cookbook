f86c506a848bee86326724aa584ed3af523712d4
Joey David <doey08@gmail.com>
2021-01-31 13:46:44 -0500

    remove bootstrap

--- web/src/index.js
+++ web/src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import NavigationMaster from './NavigationMaster'
-import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
   <React.StrictMode>
