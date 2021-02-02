38d86c3835ea71249f3075e762bdb0c89cc26ecb
Joey David <doey08@gmail.com>
2021-02-01 23:00:50 -0500

    rework allowing for multiple lists (almost done)

--- web/src/ShoppingList.js
+++ web/src/ShoppingList.js
@@ -5,6 +5,19 @@ import DeleteIcon from '@material-ui/icons/Delete';
 import UpIcon from '@material-ui/icons/ArrowUpward';
 import DownIcon from '@material-ui/icons/ArrowDownward';
 import Paper from '@material-ui/core/Paper';
+import Button from '@material-ui/core/Button';
+
+// Pop-up dialog box
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogTitle from '@material-ui/core/DialogTitle';
+
+// Select stuff
+import FormControl from '@material-ui/core/FormControl';
+import Select from '@material-ui/core/Select';
+import InputLabel from '@material-ui/core/InputLabel';
+import MenuItem from '@material-ui/core/MenuItem';
 
 // Table stuff
 import Table from '@material-ui/core/Table';
@@ -14,8 +27,10 @@ import TableContainer from '@material-ui/core/TableContainer';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 
-import { IconButton } from '@material-ui/core';
-import GetCookie from './GetCookie';
+import { Grid, IconButton } from '@material-ui/core';
+import GetCookie from './common-functions/GetCookie';
+
+import './css/ShoppingList.css';
 
 class ShoppingListForm extends React.Component {
     constructor(props) {
@@ -23,39 +38,57 @@ class ShoppingListForm extends React.Component {
         this.state = {
             item: '',
             quantity: 1,
-            list_contents: [],
+            saved_lists: [{
+                name: 'My List',
+                content: [],
+            }],
+            current_list_index: 0,
 
             item_error_text: '',
             item_error: false,
 
             qty_error_text: '',
             qty_error: false,
+
+            show_add_list: false,
         }
 
         this.handleChange = this.handleChange.bind(this);
+        this.handleSelectChange = this.handleSelectChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
         this.deleteEntry = this.deleteEntry.bind(this);
         this.incrementQty = this.incrementQty.bind(this);
         this.decrementQty = this.decrementQty.bind(this);
         this.updateListCookie = this.updateListCookie.bind(this);
+        this.toggleCreateList = this.toggleCreateList.bind(this);
+        this.handleSubmitNewList = this.handleSubmitNewList.bind(this);
     }
 
     componentDidMount() { // Load list from cookie if present
-        const cookie_shopping_list_str = GetCookie('shopping_list');
-        if (cookie_shopping_list_str !== '') {
-            const shopping_list = JSON.parse(cookie_shopping_list_str);
-            this.setState({list_contents:shopping_list});
+        const cookie_saved_lists_str = GetCookie('saved_lists');
+        const cookie_current_list_index = GetCookie('current_list_index');
+        if (cookie_saved_lists_str !== '') {
+            const saved_lists_cookie = JSON.parse(cookie_saved_lists_str);
+            this.setState({saved_lists:saved_lists_cookie});
         } else {
-            console.log('No shopping_list cookie present');
+            console.log('No saved_lists cookie present');
+        }
+
+        if (cookie_current_list_index !== '') {
+            const current_list_index_cookie = JSON.parse(cookie_current_list_index)
+            this.setState({current_list_index:current_list_index_cookie})
+        } else {
+            console.log('No current_list_index cookie present');
         }
     }
 
     updateListCookie() {
-        let oneYearFromNow = new Date(new Date().setFullYear(new Date().getFullYear() + 1)) // set cookie to expire one year from now
-        document.cookie = "shopping_list=" + JSON.stringify(this.state.list_contents) + "; expires=" + oneYearFromNow;
+        const oneYearFromNow = new Date(new Date().setFullYear(new Date().getFullYear() + 1)) // set cookie to expire one year from now
+        document.cookie = "saved_lists=" + JSON.stringify(this.state.saved_lists) + "; expires=" + oneYearFromNow;
+        document.cookie = "current_list_index=" + JSON.stringify(this.state.current_list_index);
     }
 
-    handleChange(event) {
+    handleChange(event) { // handle change for our add item form
         const target = event.target;
         const value = target.value;
         const name = target.name;
@@ -69,14 +102,20 @@ class ShoppingListForm extends React.Component {
         })
     }
 
-    handleSubmit(event) {
+    handleSelectChange(event) { //handle change for select list box
+        this.setState({current_list_index: event.target.value})
+    }
+
+    handleSubmit(event) { //when submitting add item form
         event.preventDefault();
         const entry = {
             item: this.state.item,
             quantity: parseInt(this.state.quantity, 10),
         };
 
-        let current_list = this.state.list_contents;
+        const current_list_index = this.state.current_list_index;
+
+        let current_list = this.state.saved_lists[current_list_index].content;
 
         let list_items_only = [];
         for (let i = 0; i < current_list.length; i++) {
@@ -88,10 +127,13 @@ class ShoppingListForm extends React.Component {
             
             current_list.push(entry);
 
+            let saved_lists_new = this.state.saved_lists;
+            saved_lists_new[current_list_index].content = current_list;
+
             this.setState({
                 item: '',
                 quantity: 1,
-                list_contents: current_list,
+                saved_lists: saved_lists_new,
                 item_error: false,
                 item_error_text: '',
                 qty_error: false,
@@ -114,34 +156,40 @@ class ShoppingListForm extends React.Component {
         }
     }
 
-    deleteEntry(list_entry) {
-        let current_items_list = this.state.list_contents; // get our current list
+    deleteEntry(list_entry) { // delete a submitted item
+        let current_items_list = this.state.saved_lists[this.state.current_list_index].content; // get our current list
         let index = current_items_list.indexOf(list_entry); // get index of value to remove
         if (index !== -1) { // In case index came up as not present
             current_items_list.splice(index, 1); // remove from the list
         }
 
+        let saved_lists_new = this.state.saved_lists
+        saved_lists_new[this.state.current_list_index].content = current_items_list
+
         this.setState({
-            list_contents: current_items_list
+            saved_lists: saved_lists_new,
         });
         this.updateListCookie()
     }
 
     incrementQty(list_entry) {
-        let current_items_list = this.state.list_contents; // get our current list
+        let current_items_list = this.state.saved_lists[this.state.current_list_index].content; // get our current list
         let index = current_items_list.indexOf(list_entry); // get index of value to adjust
         if (index !== -1) {
             current_items_list[index].quantity += 1 // increase item qty by 1
         }
 
+        let saved_lists_new = this.state.saved_lists
+        saved_lists_new[this.state.current_list_index].content = current_items_list
+
         this.setState({
-            list_contents: current_items_list
+            saved_lists: saved_lists_new,
         })
         this.updateListCookie()
     }
 
     decrementQty(list_entry) {
-        let current_items_list = this.state.list_contents; // get our current list
+        let current_items_list = this.state.saved_lists[this.state.current_list_index].content; // get our current list
         let index = current_items_list.indexOf(list_entry); // get index of value to adjust
         if (index !== -1) {
             if (current_items_list[index].quantity > 1) {
@@ -149,16 +197,96 @@ class ShoppingListForm extends React.Component {
             }
         }
 
+        let saved_lists_new = this.state.saved_lists
+        saved_lists_new[this.state.current_list_index].content = current_items_list
+
         this.setState({
-            list_contents: current_items_list
+            saved_lists: saved_lists_new,
         })
         this.updateListCookie()
+    }
+
+    toggleCreateList(event) {
+        if (this.state.show_add_list === true) {
+            this.setState({show_add_list:false})
+        } else {
+            this.setState({show_add_list:true})
+        }
+    }
+
+    handleSubmitNewList(event) {
+        event.preventDefault();
+
+        console.log(event.target.value);
+
+        const new_list = {
+            name: 'submitted_name',
+            content: []
+        };
+
+        let saved_lists_new = this.state.saved_lists;
+        const new_list_index = saved_lists_new.push(new_list) - 1;
+        console.log(new_list_index)
+        this.setState({
+            show_add_list: false,
+            saved_lists: saved_lists_new,
+            current_list_index: new_list_index,
+        })
     }
 
     render() {
         return (
             <div id="shopping-list-container">
-            <h1>Shopping List</h1>
+            <Grid container direction="row" alignItems="center">
+                <Grid item>
+                <h1>Shopping List</h1>
+                </Grid>
+                <Grid item style={{marginLeft:25}}>
+                <br />
+                <FormControl variant="outlined">
+                    <InputLabel htmlFor="outlined-age-native-simple">Current List</InputLabel>
+                    <Select
+                    className="select-current-list"
+                    value={this.state.current_list_index}
+                    onChange={this.handleSelectChange}
+                    label="Current List"
+                    >
+                    {this.state.saved_lists.map((saved_list, index) => (
+                        <MenuItem key={index} value={index}>{saved_list.name}</MenuItem>
+                    ))}
+                    </Select>
+                </FormControl>
+                </Grid>
+                <Grid item style={{marginTop:20,marginLeft:7}}>
+                    <IconButton type="button" onClick={this.toggleCreateList}><AddIcon /></IconButton>
+                </Grid>
+            </Grid>
+
+            <Dialog open={this.state.show_add_list} onClose={this.toggleCreateList} aria-labelledby="form-dialog-title">
+                <DialogTitle id="form-dialog-title">New Shopping List</DialogTitle>
+                <form>
+                <DialogContent>
+                <TextField
+                    autoFocus
+                    margin="dense"
+                    label="List Name"
+                    type="text"
+                    fullWidth
+                />
+                </DialogContent>
+                <DialogActions>
+                <Button type="button" onClick={this.toggleCreateList} color="primary">
+                    Cancel
+                </Button>
+                <Button type="submit" onClick={this.handleSubmitNewList} color="primary">
+                    Create
+                </Button>
+                </DialogActions>
+                </form>
+            </Dialog>
+
+            <br />
+            
             <form onSubmit={this.handleSubmit}>
                 <TextField variant="standard" name="item" id="input_item" label="Item" 
                     onChange={this.handleChange} value={this.state.item}
@@ -185,7 +313,7 @@ class ShoppingListForm extends React.Component {
                         </TableRow>
                     </TableHead>
                     <TableBody>
-                        {this.state.list_contents.map((list_entry) => (
+                        {this.state.saved_lists[this.state.current_list_index].content.map((list_entry) => (
                         <TableRow key={list_entry['item']}>
                             <TableCell id={"id_item_" + list_entry['item']}>{list_entry['item']}</TableCell>
                             <TableCell>{list_entry['quantity']}</TableCell>
