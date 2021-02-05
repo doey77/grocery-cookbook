66762920c94891fb1fc1d8cb5993677c8fee1348
Joey David <doey08@gmail.com>
2021-02-04 22:43:22 -0500

    semicolon

--- web/src/index.js
+++ web/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import NavigationMaster from './NavigationMaster'
+import NavigationMaster from './NavigationMaster';
 
 ReactDOM.render(
   <NavigationMaster></NavigationMaster>,
