bffb799e359484a0b8d7fb1fe6b0f50e229ba904
Joey David <doey08@gmail.com>
2021-02-08 23:29:38 -0500

    extra descriptions for docs, delete unused

--- api/main.py
+++ api/main.py
@@ -5,8 +5,28 @@ from .core.base_model import Base
 
 from .shoppinglists.routes import router as shoppinglists_router
 
-app = FastAPI()
+tags_metadata = [
+    {
+        "name": "default",
+        "description": "Basic operations"
+    },
+    {
+        "name": "shoppinglists",
+        "description": "Operations for interacting with shopping lists",
+    }
+]
+
+app = FastAPI(
+    title="Grocery Cookbook",
+    description="This is the API for Grocery Cookbook, built with FastAPI",
+    version="1.0.0",
+    openapi_tags=tags_metadata,
+)
 
 app.include_router(shoppinglists_router)
+
+@app.get("/")
+def greeting():
+    return {"message": "Welcome to the Grocery Cookbook API. Browse docs at /docs"}
 
 Base.metadata.create_all(bind=engine)
