2f7f91eb0438c3e353b0cc99b61d41abd000054d
Joey David <doey08@gmail.com>
2021-02-14 17:45:33 -0500

    add cors

--- api/main.py
+++ api/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
 
 from .core.database import engine, DBBase
 from .core.settings import settings
@@ -36,6 +37,15 @@ app = FastAPI(
     openapi_tags=tags_metadata,
     openapi_url=f"{settings.API_V1_STR}/openapi.json",
 )
+
+if settings.BACKEND_CORS_ORIGINS:
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=[str(origin) for origin in settings.BACKEND_CORS_ORIGINS],
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
 
 @app.get("/")
 def base_domain_greeting():
