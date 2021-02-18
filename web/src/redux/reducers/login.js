d10c43594217ee4e68d1b46ce73c9b6d2bfe2ea0
Joey David <doey08@gmail.com>
2021-02-17 22:21:18 -0500

    building out redux for login

---
+++ web/src/redux/reducers/login.js
@@ -0,0 +1,21 @@
+const LoginComponent = (state = initialState, action) => {
+    switch (action.type) {
+
+      // This reducer handles any action with type "LOGIN"
+      case "LOGIN":
+          return state.map(user => {
+              if (user.username !== action.username) {
+                  return user;
+              }
+
+              if (user.password == action.password) {
+                  return {
+                      ...user,
+                      login_status: "LOGGED IN"
+                  };
+              }
+          });
+default:
+          return state;
+      } 
+};
