583afd2457576b99041cd03850013556ad16d504
Joey David <doey08@gmail.com>
2021-03-07 17:03:54 -0500

    dialogs create/edit lists working

--- web/src/pages/ShoppingList.js
+++ web/src/pages/ShoppingList.js
@@ -33,27 +33,39 @@ import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 
 import { Grid, IconButton } from '@material-ui/core';
+
+import { useSnackbar } from 'notistack';
+
 import { getCookie } from '../services/cookies';
 
-import axios from 'axios';
 
-import { apiSettings } from '../services/apiSettings';
 import '../css/ShoppingList.css';
 
 
 function ShoppingList() {
     const wipeErrorTextField = {msg: '', err: false};
 
+    const { enqueueSnackbar, closeSnackbar } = useSnackbar();
+
     const [item, setItem] = useState('');
     const [quantity, setQuantity] = useState(1);
+
     const [currentListIndex, setCurrentListIndex] = useState(0);
     const [savedLists, setSavedLists] = useState([{
         name: 'My List', content: [],
     }]);
+
+    const [newListName, setNewListName] = useState('');
+    const [editListName, setEditListName] = useState('');
+
     const [addItemNameError, setAddItemNameError] = useState(wipeErrorTextField);
     const [addItemQtyError, setAddItemQtyError] = useState(wipeErrorTextField);
+    const [newListNameError, setNewListNameError] = useState(wipeErrorTextField);
+    const [editListNameError, setEditListNameError] = useState(wipeErrorTextField);
+
 
     const [showDialogNewList, setShowDialogNewList] = useState(false);
+    const [showDialogEditList, setShowDialogEditList] = useState(false);
 
     const handleItemChange = event => {
         setItem(event.target.value);
@@ -63,6 +75,16 @@ function ShoppingList() {
     const handleQuantityChange = event => {
         setQuantity(event.target.value);
         setAddItemQtyError(wipeErrorTextField);
+    };
+
+    const handleNewListNameChange = event => {
+        setNewListName(event.target.value);
+        setNewListNameError(wipeErrorTextField);
+    };
+
+    const handleEditListNameChange = event => {
+        setEditListName(event.target.value);
+        setEditListNameError(wipeErrorTextField);
     };
 
     const submitItem = (event) => {
@@ -96,6 +118,91 @@ function ShoppingList() {
         }
     };
 
+    /**
+     * Checks to see if list exists in savedLists
+     * @param {string} nameToCheck Name of list to check for existence
+     * @returns Bool, whether list exists or not
+     */
+    const listNameExists = (nameToCheck) => {
+        let exists = false;
+        for (let i = 0; i < savedLists.length; i++) {
+            const listName = savedLists[i].name;
+            if (nameToCheck === listName) {
+                exists = true;
+            }            
+        }
+
+        return exists;
+    };
+
+    const submitNewList = (event) => {
+        event.preventDefault();
+
+        const listExists = listNameExists(newListName);
+
+        if (listExists) {
+            setNewListNameError({msg: 'A list with name already exists', err: true});
+        } else {
+            const newList = {
+                name: newListName,
+                content: []
+            };
+            const newSavedLists = [...savedLists];
+            newSavedLists.push(newList);
+            setSavedLists(newSavedLists);
+            setCurrentListIndex(savedLists.length); // Set current list to new list
+
+            dialogCloseNewList();
+        }
+    };
+
+    const submitEditList = (event) => {
+        event.preventDefault();
+
+        const listExists = listNameExists(editListName);
+
+        if (listExists) {
+            setEditListNameError({msg: 'A list with that name already exists', err: true});
+        } else {
+            const newSavedLists = [...savedLists];
+            newSavedLists[currentListIndex].name = editListName;
+            setSavedLists(newSavedLists);
+
+            dialogCloseEditList();
+        }
+    };
+
+    const deleteList = () => {
+        if (savedLists.length < 2) {
+            // If we only have one list, don't allow delete
+            enqueueSnackbar("You must have at least one list", {variant: 'error'});
+        } else {
+            const newSavedLists = [...savedLists];
+            newSavedLists.splice(currentListIndex, 1);
+    
+            // After deleting, need to set new current list
+            // if deleting the latest list. Otherwise the current
+            // list index will just be the previous list in the savedLists
+            if (currentListIndex+1 === savedLists.length) {
+                setCurrentListIndex(currentListIndex-1);
+            }
+            setSavedLists(newSavedLists);
+            dialogCloseEditList();
+        }
+    };
+
+    const dialogCloseNewList = () => {
+        setShowDialogNewList(false);
+        setNewListName('');
+        setNewListNameError(wipeErrorTextField);
+    };
+
+    const dialogCloseEditList = () => {
+        setShowDialogEditList(false);
+        setEditListName('');
+        setEditListNameError(wipeErrorTextField);
+    };
+
     const pageHeader = (
         <Grid container direction="row" alignItems="center" justify="flex-start" spacing={1}>
             <Grid item><h1>Shopping List</h1></Grid>
@@ -121,10 +228,13 @@ function ShoppingList() {
             </FormControl>
             </Grid>
             <Grid item className="list-button">
-                <IconButton type="button" ><AddIcon /></IconButton>
+                <IconButton type="button" onClick={() => setShowDialogNewList(true)}><AddIcon /></IconButton>
             </Grid>
             <Grid item className="list-button">
-                <IconButton type="button" ><EditIcon /></IconButton>
+                <IconButton type="button" onClick={() => {
+                    setShowDialogEditList(true);
+                    setEditListName(savedLists[currentListIndex].name);
+                }}><EditIcon /></IconButton>
             </Grid>
         </Grid>
     );
@@ -179,34 +289,68 @@ function ShoppingList() {
         </TableContainer>
     );
 
-    // const dialogNewList = (
-    //     <Dialog open={false} onClose={setShowDialogNewList(false)} aria-labelledby="create-new-list">
-    //         <DialogTitle id="create-new-list">New Shopping List</DialogTitle>
-    //         <form>
-    //         <DialogContent>
-    //         <TextField
-    //             autoFocus
-    //             margin="dense"
-    //             label="List Name"
-    //             type="text"
-    //             // value={this.state.add_list_name}
-    //             // onChange={this.handleChangeNewList}
-    //             // error={this.state.add_list_name_error}
-    //             // helperText={this.state.add_list_name_error_text}
-    //             fullWidth
-    //         />
-    //         </DialogContent>
-    //         <DialogActions>
-    //         <Button type="button" onClick={setShowDialogNewList(false)} color="primary">
-    //             Cancel
-    //         </Button>
-    //         <Button type="submit" onClick={setShowDialogNewList(false)} color="primary">
-    //             Create
-    //         </Button>
-    //         </DialogActions>
-    //         </form>
-    //     </Dialog>
-    // );
+    const dialogNewList = (
+        <Dialog open={showDialogNewList} onClose={dialogCloseNewList} aria-labelledby="create-new-list">
+            <DialogTitle id="create-new-list">New Shopping List</DialogTitle>
+            <form>
+            <DialogContent>
+            <TextField
+                autoFocus
+                margin="dense"
+                label="List Name"
+                type="text"
+                value={newListName}
+                onChange={handleNewListNameChange}
+                error={newListNameError.err}
+                helperText={newListNameError.msg}
+                fullWidth
+            />
+            </DialogContent>
+            <DialogActions>
+            <Button type="button" onClick={dialogCloseNewList} color="primary">
+                Cancel
+            </Button>
+            <Button type="submit" onClick={submitNewList} color="primary">
+                Create
+            </Button>
+            </DialogActions>
+            </form>
+        </Dialog>
+    );
+
+    const dialogEditList = (
+        <Dialog open={showDialogEditList} onClose={dialogCloseEditList} aria-labelledby="edit-list">
+            <DialogTitle id="edit-list">
+            <Grid container direction="row" alignItems="center" justify="space-between">
+                <Grid item>Manage {savedLists[currentListIndex].name}</Grid>
+                <Grid item><IconButton type="button" onClick={deleteList}><DeleteIcon /></IconButton></Grid>
+            </Grid>
+            </DialogTitle>
+                <DialogContent>
+                <form>
+                <TextField
+                    autoFocus
+                    margin="dense"
+                    label={"List #" + (currentListIndex+1)}
+                    type="text"
+                    value={editListName}
+                    onChange={handleEditListNameChange}
+                    error={editListNameError.err}
+                    helperText={editListNameError.msg}
+                    fullWidth
+                />
+                <DialogActions>
+                <Button type="button" onClick={dialogCloseEditList} color="primary">
+                    Cancel
+                </Button>
+                <Button type="submit" onClick={submitEditList} color="primary">
+                    Update Name
+                </Button>
+                </DialogActions>
+                </form>
+                </DialogContent>
+        </Dialog>
+    );
 
     return (
         <>
@@ -217,7 +361,8 @@ function ShoppingList() {
             <br />
             {listTable}
 
-            {/* {dialogNewList} */}
+            {dialogNewList}
+            {dialogEditList}
         </>
     );
 }
